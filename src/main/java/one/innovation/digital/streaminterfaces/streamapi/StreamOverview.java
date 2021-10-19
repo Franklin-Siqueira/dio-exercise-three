83426ca483c8ee91ee988fa0c8642528302c7202
Franklin-Siqueira <franklin.cs@outlook.com>
2021-10-19 04:53:34 -0300

    Update: add translations @ StramOverview.

--- src/main/java/one/innovation/digital/streaminterfaces/streamapi/StreamOverview.java
+++ src/main/java/one/innovation/digital/streaminterfaces/streamapi/StreamOverview.java
@@ -7,36 +7,40 @@ public class StreamOverview {
     public static void main(String[] args) {
         List<String> randomNumbers =
                 Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
-
+        //
         System.out.println("Elements with REFERENCE METHOD (with STREAM): ");
         randomNumbers.stream()
                  .forEach(System.out::println);
-
+        //
         System.out.println("Elements with REFERENCE METHOD: ");
         randomNumbers.forEach(System.out::println);
-
+        //
         System.out.println("First 5 elements to SET | Pegue os 5 primeiros números e coloque dentro de um Set:");
         randomNumbers.stream()
-                .limit(5)
-                .collect(Collectors.toSet())
+                .limit(6)
+                .collect(Collectors
+                        .toSet())
                 .forEach(System.out::println);
         //
         Set<String> firstFiveRandomNumbers = randomNumbers.stream()
-                .limit(5)
-                .collect(Collectors.toSet());
-        System.out.println(firstFiveRandomNumbers);
+                .limit(6)
+                .collect(Collectors
+                        .toSet());
+        System.out.printf("First five random numbers: %s \n", firstFiveRandomNumbers);
         //
-        System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
+        System.out.println("Convert String List to Integer List | Transforme esta lista de String em uma lista de números inteiros.");
         List<Integer> randomNumbers1 = randomNumbers.stream()
                 .map(Integer::parseInt)
-                .collect(Collectors.toList());
+                .collect(Collectors
+                        .toList());
         //
         List<Integer> randomNumbersInteger = randomNumbers.stream()
                 .map(Integer::parseInt)
-                .collect(Collectors.toList());
+                .collect(Collectors
+                        .toList());
 
         //.forEach(System.out::println);
-        System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista:");
+        System.out.println("Create a new List of even numbers greater than 2 | Pegue os números pares e maiores que 2 e coloque em uma lista:");
         randomNumbers.stream()
                 .map(Integer::parseInt)
                 .filter(i -> i % 2 == 0 && i > 2)
@@ -65,46 +69,48 @@ public class StreamOverview {
         System.out.println(randomNumbers1);
 //        randomNumbersInteger.removeIf(i -> (i % 2 != 0));
 //        System.out.println(randomNumbersInteger);
-
-//        Para você
-        System.out.println("Ignore os 3 primeiros elementos da lista e imprima o restante:");
+        //
+        System.out.println("Print numbers, ignoring first 3 elements | Ignore os 3 primeiros elementos da lista e imprima o restante:");
         randomNumbersInteger.stream()
                 .skip(3)
                 .forEach(System.out::println);
-
-        long countNumerosUnicos = randomNumbersInteger.stream()
+        // Remove repetitions
+        long countUniqueNumbers = randomNumbersInteger.stream()
                 .distinct()
                 .count();
-        System.out.println("Retirando os números repetidos da lista, quantos números ficam? " + countNumerosUnicos);
-
-        System.out.print("Mostre o menor valor da lista: ");
+        System.out.println("Removing repetitions, what's the List length | Retirando os números repetidos da lista, quantos números ficam? " + countUniqueNumbers);
+        //
+        System.out.print("Print lower value in the List | Mostre o menor valor da lista: ");
         randomNumbersInteger.stream()
                 .mapToInt(Integer::intValue)
                 .min()
                 .ifPresent(System.out::println);
-
-        System.out.print("Mostre o maior valor da lista: ");
+        //
+        System.out.print("Print greater value in the List | Mostre o maior valor da lista: ");
         randomNumbersInteger.stream()
                 .mapToInt(Integer::intValue)
                 .max()
-                .ifPresent(System.out::println);;
-
-        int somaDosNumerosPares = randomNumbersInteger.stream()
+                .ifPresent(System.out::println);
+        //
+        int sumOfEvenNumbers = randomNumbersInteger.stream()
                 .filter(i -> (i % 2 == 0))
                 .mapToInt(Integer::intValue)
                 .sum();
-        System.out.println("Pegue apenas os números ímpares e some: " + somaDosNumerosPares);
-
-        System.out.println("Mostre a lista na ordem númerica: ");
+        System.out.println("Sum of even numbers | Pegue apenas os números pares e some: " + sumOfEvenNumbers);
+        //
+        System.out.println("Print in order | Mostre a lista na ordem númerica: ");
         List<Integer> naturalOrderNumbers = randomNumbersInteger.stream()
-                .sorted(Comparator.naturalOrder())
-                .collect(Collectors.toList());
+                .sorted(Comparator
+                        .naturalOrder())
+                .collect(Collectors
+                        .toList());
         System.out.println(naturalOrderNumbers);
-
-        System.out.println("Agrupe os valores ímpares múltiplos de 3 ou de 5:");
+        //
+        System.out.println("Group by odd numbers multiples of 3 and 5 | Agrupe os valores ímpares múltiplos de 3 ou de 5:");
 //        dica: collect(Collectors.groupingBy(new Function())
-        Map<Boolean, List<Integer>> collectNumerosMultiplosDe3E5 = randomNumbersInteger.stream()
-                .collect(Collectors.groupingBy(i -> (i % 3 == 0 || i % 5 == 0)));
-        System.out.println(collectNumerosMultiplosDe3E5);
+        Map<Boolean, List<Integer>> collectEvenNumbersDivThreeAndFive = randomNumbersInteger.stream()
+                .collect(Collectors
+                        .groupingBy(number-> (number % 3 == 0 || number % 5 == 0)));
+        System.out.println(collectEvenNumbersDivThreeAndFive);
     }
 }
